c5068829d3f56b92577a6a48a7c6ee18a960ee7d
Sameer <mhdsmr2@gmail.com>
2020-05-14 18:12:57 +0530

    Initial commit

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "git init",
+        "excerpt":"First post to setup my blogging website.  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/init/",
+        "teaser":null}]
