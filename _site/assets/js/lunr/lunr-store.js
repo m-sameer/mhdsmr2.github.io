2d198e0f4903aff67ac86d9f25426dd4de58497a
Sameer <mhdsmr2@gmail.com>
2020-05-28 03:38:58 +0530

    Increase Urdu font size

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "git init",
-        "excerpt":"First post to setup my blogging website.  ","categories": [],
+        "title": "wo jahaa'n sharaabei'n piyaa karei'n",
+        "excerpt":"وہ جہاں شرابیں پیا کریں، وہ ہی دورِ قہرِ نظر گیا رہے جس ہی راہ میں مبتلا، وہ ہی بابِ راہِ سفر گیا کبھی یاں ہمیں تھے نِرے گلے، کبھی وہ ہی شکوے سے تر گیا کبھی دونوں جو ہوئے راضی سے، تو وہ عشق ہی تھا گزر گیا مجھے...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/init/",
+        "url": "http://localhost:4000/ghazal1/",
         "teaser":null}]
